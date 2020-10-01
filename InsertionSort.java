3afb19ba0eb995afa345eca16bd0014743609902
lmoerynck <71604654+lmoerynck@users.noreply.github.com>
2020-10-01 22:49:16 +0200

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,15 +1,9 @@
 public class InsertionSort {
     public static void main(String args[]){
         int data[] = {4,6,2,9,1,-65,987};
-
         sort(data);
     }
-
     public static void sort(int data[]){
-
-        System.out.print("Contenu du tableau avant le tri: ");
-        for (int w = 0; w < data.length; w++) System.out.print(data[w] + " ");
-
         for (int i = 0; i < data.length; i++){
             for (int j = i + 1; j < data.length; j++){
                 if (data[j] < data[i]){
@@ -20,7 +14,5 @@ public class InsertionSort {
                 }
             }
         }
-        System.out.print("\nContenu du tableau après le tri: ");
-        for (int w = 0; w < data.length; w++) System.out.print(data[w] + " ");
     }
 }
